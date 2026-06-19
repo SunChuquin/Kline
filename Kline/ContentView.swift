ccc172f5295c97406a04a58a7df71a130a546435
sunck <1715216365@qq.com>
2026-06-19 12:02:23 +0800

    Initial Commit

---
+++ Kline/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kline
+//
+//  Created by 孙楚昆 on 2026/6/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Kline/KlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  KlineApp.swift
+//  Kline
+//
+//  Created by 孙楚昆 on 2026/6/19.
+//
+
+import SwiftUI
+
+@main
+struct KlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
