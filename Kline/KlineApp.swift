2d24696d8632d0c113aa35633f8e12120be51e07
sunck <1715216365@qq.com>
2026-08-27 14:55:23 +0800

    添加 appVersion 启动日志，便于识别已安装的构建版本

--- Kline/KlineApp.swift
+++ Kline/KlineApp.swift
@@ -9,6 +9,13 @@ import SwiftUI
 
 @main
 struct KlineApp: App {
+    /// 应用版本号，便于通过设备日志识别已安装的构建
+    static let appVersion = "1.0.1"
+
+    init() {
+        print("[Kline] 启动构建版本: \(KlineApp.appVersion)")
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
