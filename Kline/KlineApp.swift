6a9afae4d4d12b7dcb839e29809e8618e4c33894
sunck <1715216365@qq.com>
2026-09-03 20:08:05 +0800

    iPad锁横屏启动、iPhone锁竖屏启动（pbxproj方向配置+AppDelegate双重锁定）

--- Kline/KlineApp.swift
+++ Kline/KlineApp.swift
@@ -2,13 +2,28 @@
 //  KlineApp.swift
 //  Kline
 //
-//  Created by 孙楚昆 on 2026/6/19.
+//  Created by 孙楚坤 on 2026/6/19.
 //
 
 import SwiftUI
+import UIKit
+
+/// 方向锁定：iPad 仅横屏（左右两个横屏方向可自由切换），iPhone 仅竖屏。
+/// 与 pbxproj 的 INFOPLIST_KEY_UISupportedInterfaceOrientations_* 双重保险，
+/// 启动即正确方向，用户旋转设备也不会翻转。
+final class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return UIDevice.current.userInterfaceIdiom == .pad
+            ? [.landscapeLeft, .landscapeRight]
+            : [.portrait]
+    }
+}
 
 @main
 struct KlineApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     /// 应用版本号，便于通过设备日志识别已安装的构建
     static let appVersion = "1.0.1"
 
